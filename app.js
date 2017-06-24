b06afdcabc7ca9bc7507b2983b5207d3c96b5dd3
hometownzero <bradycpeters@gmail.com>
2017-06-23 23:27:39 -0500

    learning js through building a note taking app

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('starting app.js');
+//load our modules
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes.js');
+
+
+
---
+++ notes.js
@@ -0,0 +1,12 @@
+console.log('Starting notes.js');
+
+// module.exports.age = 23;
+
+//learning to create functions and export with the module
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New Note';
+};
+
+
+
