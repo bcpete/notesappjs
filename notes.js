a7a74f65ac0cfdd3d88f2d6cb1dd0a15a8f7fdf7
hometownzero <bradycpeters@gmail.com>
2017-06-26 17:55:49 -0500

    filled out the add note function. Now writes json to notes-data.json, checks for duplicate titles. Need to refactor.

--- notes.js
+++ notes.js
@@ -1,7 +1,30 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+//Take inpute from command line, create note object, add it to the notes array,
+//write the array to the notes-data.json folder
 var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    }catch(e){
+
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    if(duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFile('notes-data.json', JSON.stringify(notes));
+    }else{
+        console.log('Title already taken. Please choose a different title');
+    }
 };
 
 var getAll = () => {
